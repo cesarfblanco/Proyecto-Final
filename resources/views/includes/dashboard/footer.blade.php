b9bad9c10320e801712ff6e10061972e51371c37
cesarfblanco <cesarfblanco97@gmail.com>
2023-02-09 20:20:58 +0100

    Login y Registro Realizado. iniciando dashboard

---
+++ resources/views/includes/dashboard/footer.blade.php
@@ -0,0 +1,23 @@
+<footer class="footer">
+    <div class="row align-items-center justify-content-xl-between">
+      <div class="col-xl-6">
+        <div class="copyright text-center text-xl-left text-muted">
+          &copy; 2018 <a href="#" class="font-weight-bold ml-1" >{{ config('app.name') }}</a>
+        </div>
+      </div>
+      <div class="col-xl-6">
+        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+          <li class="nav-item">
+            <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link" target="_blank">Quienes Somos</a>
+          </li>
+          <li class="nav-item">
+            <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </footer>
